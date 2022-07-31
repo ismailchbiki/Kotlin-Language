574fede4eeca2250ecc1ad347fb0b06fad1445ba
Ismail Chbiki <ismailchbiki.nl@gmail.com>
2022-07-31 23:16:32 +0200

    kotlin programming language: 26:12 loops - coding master

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,53 @@
+//GUIDELINES FOR KOTLIN PROGRAMMING LANGUAGE:
+/*
+ -> No need to create functions inside a class (can be at a file level)
+ -> The difference between val and var is that val is immutable and var is mutable (can be changed)
+ -> No need to specify the data type of variable (var) or value (val) (can be inferred) (like JavaScript)
+
+ * Kotlin lang feature over Java:
+ -> Null Safety Feature is a feature where u need to declare null vars explicitly (e.g. var name: String? = "Ismail")
+
+ * String Templates:
+    -> String templates are a way to concatenate strings in Kotlin (e.g. "Hello $name")
+    -> For complex operations, curly braces are used (e.g. "Hello ${1 + 2}")
+*/
+
+fun main() {
+    val name = "Ismail" // same code as above
+    var lastName: String? = "Chbiki" // nullable
+
+    lastName = null // nullable
+    println("Hello $name")
+    println("Hello ${1 + 2}") //for more complex code use curly braces
+
+    //Arrays:
+    val arr = arrayOf(150, 250, 350, 450, 550) // array of integers
+    println("Position 0 in the array: ${arr[0]}")
+
+    //Conditional Statements:
+//    val msg = "Enter your age:"
+//    val age = Integer.valueOf(readLine()) //converting to integer
+//    if (age > 20) println("over twenties")
+//    else println("less then twenties")
+
+    // if statements in Kotlin can be a return type:
+//    val result = if(age > 20) "Hello" else "Bye"
+//    print(result)
+
+    //when statements:
+    val day = Integer.valueOf(readLine())
+//    val result = when (day) { //the result can also be assigned to a var
+//        1, 10 -> "One or ten"
+//        2 -> "Two"
+//        3 -> "Three"
+//        else -> "No Num"
+//    }
+
+    //above code written differently:
+    val result = when { //when without args
+        day == 1 -> "One"
+        day == 2 -> "Two"
+        else -> "Other num"
+    }
+    println(result)
+}
